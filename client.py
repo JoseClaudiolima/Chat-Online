a34301fb1d8e3d5c225afcfa4764ec19424e172f
José Cláudio <jose.c.lima.sp@gmail.com>
2024-04-11 11:47:49 -0300

    Aperfeiçoamento no gerenciamento de janelas

--- client.py
+++ client.py
@@ -1,5 +1,6 @@
-import tkinter as tk
 from Lib import CriptografiaRSA as rsa
+
+import tkinter as tk
 import ttkbootstrap as ttk
 import socket
 import threading
@@ -11,9 +12,54 @@ import math
 #Passo 4: Coloque uma porta desejada, acho que entre #1025 a 49152 não da erro, mas talvez devemos pesquisar isso
 #Passo 5: Colocaria a senha, porém ainda não tem funcionalidade para ela
 
+#comando = criar janela ou delete e crie
+def Gerenciar_Janela(comando,config_janela,titulo):
+    def Destroir_widgets():
+        for widget in window.winfo_children():
+            widget.destroy()
+        return
+
+    def Janela_title_geometry(janela_personalizada,config_janela):
+        x,y = 0,0
+        if config_janela is None:
+            config_janela = {'dimensoes' : '300x127', 'alinhamento_tela': 'centralizado'}
+
+        if config_janela['alinhamento_tela'] == 'centralizado':
+            # Calculando a posição central da tela
+            screen_width = janela_personalizada.winfo_screenwidth()
+            screen_height = janela_personalizada.winfo_screenheight()
+            x = (screen_width - 400) // 2
+            y = (screen_height - 200) // 2
+
+        janela_personalizada.geometry(f"{config_janela['dimensoes']}+{x}+{y}")
+        janela_personalizada.title(titulo)
+
+        return janela_personalizada
+    
+
+    if comando == 'Crie':
+        global window
+        window = ttk.Window()
+
+    elif comando == 'Delete e crie':
+        Destroir_widgets()
+
+    if comando == 'Crie' or comando == 'Delete e crie':
+        window = Janela_title_geometry(window,config_janela) 
+        return window
+
+    if comando == 'Crie-Toplevel':
+        #global window_Toplevel  #Acho que da para tirar isso
+        window_Toplevel = ttk.Toplevel()    # Usar Toplevel em vez de Window, evita msg de erro (Cria uma janela mais independente, pelo que entendi) 
+        window.withdraw()                   # usa witdraw em vez de destroy, evita msg de erro (isso faz a tela não aparecer para o usuario em vez de destroila)
+       
+        window_Toplevel = Janela_title_geometry(window_Toplevel,config_janela)  
+        return window_Toplevel
+
+ 
 
 #fazendo assim para ir logo, dps junta o grupo com chat direto
-def Chat_App(nmr_porta,senha,qtd_pessoas,window):
+def Chat_App(nmr_porta,senha,qtd_pessoas):
     def Enviar_mensagem():
         mensagem = f"{message_input.get()} {format(math.pi, '.10f')} {name}"
         if mensagem:
@@ -56,12 +102,10 @@ def Chat_App(nmr_porta,senha,qtd_pessoas,window):
     cliente_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     cliente_socket.connect((ip_server, int(nmr_porta)))
     
-    window.withdraw() # usa witdraw em vez de destroy, evita msg de erro (isso faz a tela não aparecer para o usuario em vez de destroila)
-    chat_window = tk.Toplevel()  # Usar Toplevel em vez de Window, evita msg de erro (Cria uma janela mais independente, pelo que entendi)
-    
-    chat_window.geometry(f'800x500')    
-    
-    chat_window.title(f"Chat Online")
+
+    chat_window = Gerenciar_Janela('Crie-Toplevel',
+                                   {'dimensoes': '800x500','alinhamento_tela': 'nenhum' },
+                                   'Chat Online')  
 
     chat_box = tk.Frame(chat_window)
     input = tk.Frame(chat_window)
@@ -89,12 +133,11 @@ def Chat_App(nmr_porta,senha,qtd_pessoas,window):
     threading.Thread(target=Receber_mensagens).start() 
 
 
-def direct_chat(window,box):
-
-    box.destroy()
-
-    window.title('Configuração - Chat-direto')
-    window.geometry('300x200')
+def direct_chat(box):
+    window = Gerenciar_Janela('Delete e crie',
+                     {'dimensoes' : '300x200', 'alinhamento_tela': 'centralizado'},
+                     'Configuração - Chat-direto')
+    
     box = tk.Frame(window)
     box.pack()    
     box2 = tk.Frame(box)
@@ -115,24 +158,23 @@ def direct_chat(window,box):
     input_senha = ttk.Entry(box3)
     input_senha.pack()
 
-    confirm_button = ttk.Button(box,text='Confirmar', command=lambda: Chat_App(input_porta.get(),input_senha.get(),2,window)).pack(pady=(15))
+    confirm_button = ttk.Button(box,text='Confirmar', command=lambda: Chat_App(input_porta.get(),input_senha.get(),2))
+    confirm_button.pack(pady=(15))
 
   
-def Inicio(window, box):
-    window.title('Escolha de chat')    
-    box.destroy()
-
+def Inicio():
+    window = Gerenciar_Janela('Delete e crie',None,'Escolha de chat')
     box = tk.Frame(window)
     box.pack()
 
-    direct_chat_button = ttk.Button(box, text='Conexão direta',command= lambda:direct_chat(window,box))
+    direct_chat_button = ttk.Button(box, text='Conexão direta',command= lambda:direct_chat(box))
     multiple_chat_button = ttk.Button(box, text='Criar Grupo')
 
     direct_chat_button.pack(pady=(20,10))
     multiple_chat_button.pack()
 
 
-def Conectar_ao_servidor(window, name_entry, box, box2):
+def Conectar_ao_servidor(name_entry):
     def Teste_conexão():
         global ip_server
         ip_server = input_ip_servidor.get()
@@ -144,20 +186,19 @@ def Conectar_ao_servidor(window, name_entry, box, box2):
             #Adicionar tela de loading aqui antes de tentar conexão, atualmente o app trava se colocar invalido
             connection.connect((ip_server, 3000))
 
-            Inicio(window, box)
+            Inicio()
         except (ConnectionRefusedError, TimeoutError, OSError):
             #Adicionar tela de erro
             print(f'Ip de servidor está errado!')
         return
+    
+
     global name
     name = name_entry.get()
     if name == '':
         return    
 
-    box.destroy()
-    box2.destroy()
-    
-    window.title('Conecte ao Servidor')
+    window = Gerenciar_Janela('Delete e crie',None,'Conecte ao Servidor')
 
     box = tk.Frame(window)
     box.pack()
@@ -172,8 +213,10 @@ def Conectar_ao_servidor(window, name_entry, box, box2):
     button_servidor.pack(pady=10)
 
 
-def Entrada(window):
-    window.title("Nome do Usuário")
+def Entrada():
+    window = Gerenciar_Janela('Crie',
+                              {'dimensoes':'300x127', 'alinhamento_tela' : 'centralizado'},
+                              "Nome do Usuário")
 
     style = ttk.Style()
     style.configure('TLabel', font=('Arial', 10, 'bold'))
@@ -184,7 +227,7 @@ def Entrada(window):
 
     label = ttk.Label(box, text="Digite seu nome:")
     name_entry = ttk.Entry(box)
-    enter_button = ttk.Button(box2, text="Confirmar", command=lambda: Conectar_ao_servidor(window, name_entry, box, box2))
+    enter_button = ttk.Button(box2, text="Confirmar", command=lambda: Conectar_ao_servidor(name_entry))
 
     box.pack(pady=15)
     box2.pack()
@@ -192,18 +235,9 @@ def Entrada(window):
     name_entry.pack(side='left')
     enter_button.pack()
 
+    window.mainloop()
     window.protocol("WM_DELETE_WINDOW", lambda: window.quit())  # Fechar janela principal sem erro
           
 
 if __name__ == '__main__':
-    Start_window = ttk.Window()
-
-    # Calculando a posição central da tela
-    screen_width = Start_window.winfo_screenwidth()
-    screen_height = Start_window.winfo_screenheight()
-    x = (screen_width - 400) // 2
-    y = (screen_height - 200) // 2
-    Start_window.geometry(f'300x127+{x}+{y}')
-
-    entry_app = Entrada(Start_window)
-    Start_window.mainloop()
+    Entrada()
