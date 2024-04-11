00e8740a56cfe005e7c089a2fdf614315f967527
José Cláudio <jose.c.lima.sp@gmail.com>
2024-04-10 23:50:21 -0300

    Adicionado janelas e alteracao na logica para se conectar ao servidor

--- client.py
+++ client.py
@@ -5,47 +5,22 @@ import socket
 import threading
 import math
 
-def Entrada_de_nomeapp(window):
+#Passo 1: Coloque o nome
+#Passo 2: Coloque o ipv4 do server, aparece no terminal do server.py
+#Passo 3: Selecione a opção de cima, a de baixo ainda não foi programada
+#Passo 4: Coloque uma porta desejada, acho que entre #1025 a 49152 não da erro, mas talvez devemos pesquisar isso
+#Passo 5: Colocaria a senha, porém ainda não tem funcionalidade para ela
 
-    def iniciar_chat(window,name_entry):
-        nome_usuario = name_entry.get()
-        if nome_usuario:
-            window.withdraw()  # Esconde a janela de entrada de nome
-            chat_window = tk.Toplevel()  # Usar Toplevel em vez de Window
-            #chat_window.geometry(f'440x250+{x}+{y}')    
-            chat_window.geometry(f'800x500+{x}+{y}')
-            ChatApp(chat_window, nome_usuario)  
 
-    window.title("Nome do Usuário")
-
-    style = ttk.Style()
-    style.configure('TLabel', font=('Arial', 10, 'bold'))
-    style.configure('TButton', font=('Arial', 10))
-
-    box = tk.Frame(window)
-
-    label = ttk.Label(box, text="Digite seu nome:")
-    name_entry = ttk.Entry(box)
-    enter_button = ttk.Button(window, text="Confirmar", command=lambda: iniciar_chat(window,name_entry))
-
-    box.pack(pady = 15)
-    label.pack(side = 'left', padx = 5)
-    name_entry.pack(side = 'right')
-    enter_button.pack()
-          
-
-def ChatApp(window, nome_usuario):
+#fazendo assim para ir logo, dps junta o grupo com chat direto
+def Chat_App(nmr_porta,senha,qtd_pessoas,window):
     def Enviar_mensagem():
-        ''' Abaixo tem 3 parametros: 
-            I. a mensagem em si; 
-            II. o delimitador de paraemtro (para separar no recebimento de mensagem, qual é cada parametro);
-            III. o nome do cliente.'''
         mensagem = f"{message_input.get()} {format(math.pi, '.10f')} {name}"
         if mensagem:
             cifra = rsa.cifrar(mensagem,28837,40301) #Está com chave já selecionada, podemos aleatorizar depois
             cliente_socket.send(cifra.encode())
             message_input.delete(0, tk.END)
-    
+
     def Receber_mensagens():
         while True:
             try:
@@ -74,11 +49,22 @@ def ChatApp(window, nome_usuario):
         """Rola o chat para baixo."""
         chat_display.yview_moveto(1.0)
 
-    name = nome_usuario
-    window.title(f"Chat Online - {name}")
+    #Abaixo a gente manda para o server, a mensagem contendo os 3 parametros abaixo, para o chat criar um novo chat lá com base nessas info
+    message = f'{nmr_porta}+{senha}+{qtd_pessoas}'
+    connection.send(message.encode())
 
-    chat_box = tk.Frame(window)
-    input = tk.Frame(window)
+    cliente_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    cliente_socket.connect((ip_server, int(nmr_porta)))
+    
+    window.withdraw() # usa witdraw em vez de destroy, evita msg de erro (isso faz a tela não aparecer para o usuario em vez de destroila)
+    chat_window = tk.Toplevel()  # Usar Toplevel em vez de Window, evita msg de erro (Cria uma janela mais independente, pelo que entendi)
+    
+    chat_window.geometry(f'800x500')    
+    
+    chat_window.title(f"Chat Online")
+
+    chat_box = tk.Frame(chat_window)
+    input = tk.Frame(chat_window)
 
     chat_display = ttk.Text(chat_box,wrap='word',state='disabled',height=10,width=120)
     chat_display.pack(side = 'left',padx=10)
@@ -99,14 +85,115 @@ def ChatApp(window, nome_usuario):
     clear_button.pack(side='left',padx=7)
 
     chat_box.pack(fill='x', padx=10, pady=10)
-    input.pack(fill='x', padx=10, pady=10)
+    input.pack(fill='x', padx=10, pady=10) 
+    threading.Thread(target=Receber_mensagens).start() 
 
-    cliente_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    #socket.connect(('192.168.15.41', 3000))
-    cliente_socket.connect(('192.168.15.162', 3000))
+
+def direct_chat(window,box):
+
+    box.destroy()
+
+    window.title('Configuração - Chat-direto')
+    window.geometry('300x200')
+    box = tk.Frame(window)
+    box.pack()    
+    box2 = tk.Frame(box)
+    box2.pack()
+    box3 = tk.Frame(box)
+    box3.pack()
+
+
+    label_porta = ttk.Label(box2,text='Escolha uma porta de rede: ')
+    label_porta.pack(pady=(10,5))
+
+    input_porta = ttk.Entry(box2)
+    input_porta.pack()
     
-    threading.Thread(target=Receber_mensagens).start()       
+    label_senha = ttk.Label(box3,text='Escolha a senha da rede:')
+    label_senha.pack(pady=(10,5))
 
+    input_senha = ttk.Entry(box3)
+    input_senha.pack()
+
+    confirm_button = ttk.Button(box,text='Confirmar', command=lambda: Chat_App(input_porta.get(),input_senha.get(),2,window)).pack(pady=(15))
+
+  
+def Inicio(window, box):
+    window.title('Escolha de chat')    
+    box.destroy()
+
+    box = tk.Frame(window)
+    box.pack()
+
+    direct_chat_button = ttk.Button(box, text='Conexão direta',command= lambda:direct_chat(window,box))
+    multiple_chat_button = ttk.Button(box, text='Criar Grupo')
+
+    direct_chat_button.pack(pady=(20,10))
+    multiple_chat_button.pack()
+
+
+def Conectar_ao_servidor(window, name_entry, box, box2):
+    def Teste_conexão():
+        global ip_server
+        ip_server = input_ip_servidor.get()
+        if ip_server == '':
+            return
+        try:
+            global connection
+            connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            #Adicionar tela de loading aqui antes de tentar conexão, atualmente o app trava se colocar invalido
+            connection.connect((ip_server, 3000))
+
+            Inicio(window, box)
+        except (ConnectionRefusedError, TimeoutError, OSError):
+            #Adicionar tela de erro
+            print(f'Ip de servidor está errado!')
+        return
+    global name
+    name = name_entry.get()
+    if name == '':
+        return    
+
+    box.destroy()
+    box2.destroy()
+    
+    window.title('Conecte ao Servidor')
+
+    box = tk.Frame(window)
+    box.pack()
+
+    label_servidor = ttk.Label(box, text='Insira o ipv4 do servidor abaixo:')
+    label_servidor.pack(pady=5)
+
+    input_ip_servidor = ttk.Entry(box)
+    input_ip_servidor.pack()
+
+    button_servidor = ttk.Button(box, text='Confirmar',command= lambda:Teste_conexão())
+    button_servidor.pack(pady=10)
+
+
+def Entrada(window):
+    window.title("Nome do Usuário")
+
+    style = ttk.Style()
+    style.configure('TLabel', font=('Arial', 10, 'bold'))
+    style.configure('TButton', font=('Arial', 10))
+
+    box = tk.Frame(window)
+    box2 = tk.Frame(window)
+
+    label = ttk.Label(box, text="Digite seu nome:")
+    name_entry = ttk.Entry(box)
+    enter_button = ttk.Button(box2, text="Confirmar", command=lambda: Conectar_ao_servidor(window, name_entry, box, box2))
+
+    box.pack(pady=15)
+    box2.pack()
+    label.pack(side='left', padx=5)
+    name_entry.pack(side='left')
+    enter_button.pack()
+
+    window.protocol("WM_DELETE_WINDOW", lambda: window.quit())  # Fechar janela principal sem erro
+          
 
 if __name__ == '__main__':
     Start_window = ttk.Window()
@@ -118,5 +205,5 @@ if __name__ == '__main__':
     y = (screen_height - 200) // 2
     Start_window.geometry(f'300x127+{x}+{y}')
 
-    entry_app = Entrada_de_nomeapp(Start_window)
+    entry_app = Entrada(Start_window)
     Start_window.mainloop()
--- server.py
+++ server.py
@@ -2,44 +2,90 @@ import socket
 import threading
 
 clients = []
+portas = []
+
+#porta entre 1024 a 49151
+#futuramente usar essa senha para permitir acesso
+def Create_chat(nmr_porta,senha,qtd_pessoas):
+    
+    def comunicacao(socket_do_client_chat):
+        client_socket_chat.append(socket_do_client_chat)
+
+        while True:
+            try:
+                mensagem_do_chat = client_socket_create_chat.recv(1024).decode()
+                if not mensagem_do_chat:
+                    break
+                for c in client_socket_chat:
+                    try:
+                        c.send(mensagem_do_chat.encode())
+                        #print(mensagem_do_chat) Para provar que a criptografia é de ponta a ponta
+                    except ConnectionError:
+                        continue
+            except ConnectionError:
+                return
+        return
+
+    server_pareamento_direto = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    
+    #If para imepdir que portas de mesmo numero sejam criadas
+    if nmr_porta not in portas:
+        server_pareamento_direto.bind((str(ipv4_address), int(nmr_porta)))
+        server_pareamento_direto.listen(5)    
+        portas.append(nmr_porta)
+        print(f'Servidor aguardando conexões, em: {nmr_porta}')
+
+        client_chat = []
+        client_socket_chat = []
+        while len(client_chat) <int(qtd_pessoas):
+
+            client_socket_create_chat, addr = server_pareamento_direto.accept()
+            print(f'Conexão recebida de {addr[0]}:{addr[1]} no chat {nmr_porta}')
+            client_chat.append(addr[1])
+
+
+            client_handler_chat = threading.Thread(target=comunicacao, args=(client_socket_create_chat,))
+            client_handler_chat.start()
+
 
 def handle_client(client_socket):
-    clients.append(client_socket)
-    
+    clients.append(client_socket)  
     while True:
         try:
             mensagem = client_socket.recv(1024).decode()
             if not mensagem:
                 break
-            broadcast(mensagem)
+            msg = mensagem.split('+') 
+            Create_chat(msg[0],msg[1],msg[2])
         except ConnectionError:
             break
-
     clients.remove(client_socket)
     client_socket.close()
 
-def broadcast(mensagem):
-    for client in clients:
-        try:
-            client.send(mensagem.encode())
-        except ConnectionError:
-            continue
-
-def start_server():
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    hostname = socket.gethostname()    
+#Aqui é só para conectar o client no server
+def Pareamento_inicial():
+    server_pareamento_inicial = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    hostname = socket.gethostname()
+    global ipv4_address    
     ipv4_address = socket.gethostbyname(hostname)
 
-    server.bind((str(ipv4_address), 3000))
-    server.listen(5)    
-    print('Servidor aguardando conexões...')
+    server_pareamento_inicial.bind((str(ipv4_address), 3000))
+    server_pareamento_inicial.listen(5)    
+    print(f'Servidor aguardando conexões... ipv4 do server: {ipv4_address}')
+
+    global usuarios
+    usuarios = []
 
     while True:
-        client_socket, addr = server.accept()
-        print(f'Conexão recebida de {addr[0]}:{addr[1]}')
+        
+        client_socket, addr = server_pareamento_inicial.accept()
+        print(f'Conexão recebida de {addr[0]}:{addr[1]} em pareamento normal com o server')
+
+        usuarios.append(addr[1])
 
         client_handler = threading.Thread(target=handle_client, args=(client_socket,))
         client_handler.start()
+    
 
 if __name__ == '__main__':
-    start_server()
+    Pareamento_inicial()
